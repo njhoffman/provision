c8414dafe142d9f7e1ce7b5974a945c3ed3d2ef6
Nick Hoffman <njhoffman1982@gmail.com>
2023-08-10 22:06:18 -0500

    updates

--- steps/02-dotfiles.sh
+++ steps/02-dotfiles.sh
@@ -6,9 +6,11 @@ eval "$(ssh-agent -s)" && ssh-add -l -E md5
 # add ssh key to github, clone provision
 mkdir -p ~/ghq/github.com/njhoffman && cd ~/ghq/github.com/njhoffman && git clone git@github.com:njhoffman/provision.git
 
+### switch to target computer for rest of steps ###
+
 # install dotbare, clone dotbare
 git clone https://github.com/kazhala/dotbare.git ~/.dotbare
 export PATH=$PATH:$HOME/.dotbare
 export DOTBARE_DIR="$HOME/.cfg"
 export DOTBARE_TREE="$HOME"
-dotbare finit -u https://github.com/njhoffman/dotfiles.git
+dotbare finit -u git@github.com:njhoffman/dotfiles.git
--- steps/03-core.sh
+++ steps/03-core.sh
@@ -4,9 +4,6 @@
 sudo apt install add-apt-key bash-doc bison byacc build-essential locate manpages-dev pkg-config zsh # autotools-dev automake
 # install core dev libs
 sudo apt install devscripts libssl-dev libreadline-dev libsqlite3-dev libbz2-dev libevent-dev zlib1g-dev
-# install network utils
-sudo apt install blueman cifs-utils curl net-tools nmap smbclient traceroute nfs-common
-# --  sudo apt install lockd, statd, showmount, nfsstat, gssd, idmapd and mount.nfs.
 # install avahi utils
 sudo apt install avahi-autoipd avahi-daemon avahi-discover avahi-utils
 # install python and python packages
@@ -17,3 +14,6 @@ sudo apt install xsel xclip xdotool wmctrl
 sudo apt install autorandr ccze fasd grc jq most sqlite3 thefuck wdiff
 # video utils and codecs
 sudo apt install ubuntu-restricted-extras ffmpeg
+# install network utils
+sudo apt install blueman cifs-utils curl net-tools nmap smbclient traceroute nfs-common
+# --  sudo apt install lockd, statd, showmount, nfsstat, gssd, idmapd and mount.nfs.
