0f8323749440181566683be1830a41565291efe7
Nick Hoffman <njhoffman1982@gmail.com>
2023-08-10 19:40:37 -0500

    updates

--- programs/jupyter/install.sh
+++ programs/jupyter/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-pip install jupyterlab      # jupyter lab: https://github.com/jupyterlab/jupyterlab
-pip install notebook        # jupyter notebook: https://github.com/jupyter/notebook
-pip install jupyterhub      # jupyter hub: https://github.com/jupyterhub/jupyterhub
-pip install jupyter-console # jupyter console: https://github.com/jupyter/jupyter_console
-pip install voila           # voila
+pip install jupyterlab               # jupyter lab: https://github.com/jupyterlab/jupyterlab
+pip install notebook                 # jupyter notebook: https://github.com/jupyter/notebook
+pip install jupyterhub               # jupyter hub: https://github.com/jupyterhub/jupyterhub
+pip install jupyter-console          # jupyter console: https://github.com/jupyter/jupyter_console
+pip install voila                    # voila
+pip install --upgrade jupyterlab-vim # https://github.com/jupyterlab-contrib/jupyterlab-vim
---
+++ ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+partition="/dev/sda" # lsblk
+version="23.04"
+file="ubuntu-$version-desktop-amd64.iso"
+url="http://releases.ubuntu.com/$version/$file"
+# url=http://cdimage.ubuntu.com/focal/daily-live/current/focal-desktop-amd64.iso
+
+cd "$HOME/Downloads"
+wget "$url"
+umount "$partition"
+sudo dd if="$HOME/Downloads/$file" of="$partition" bs=4M oflag=sync status=progress
+
+# sudo apt install gnome-multi-writer
+# sudo apt install pv
+# sudo dd if="$HOME/Downloads/ubuntu-23.04-desktop-amd64.iso" | pv | sudo dd of="$partition" bs=4M conv=fdatasync
