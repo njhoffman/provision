304c81935301024a41057dff3b2a092a25e8086f
Nick Hoffman <njhoffman1982@gmail.com>
2023-09-10 16:56:25 -0500

    add performance test

--- programs/performancetest/install.sh
+++ programs/performancetest/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget https://www.passmark.com/downloads/pt_linux_x64.zip
+sudo apt install libncurses5
+cd ~/Downloads && wget https://www.passmark.com/downloads/pt_linux_x64.zip
+unzip pt_linux_x64.zip && sudo mv pt_linux_x64/pt_linux_x64 /usr/local/bin/pt
