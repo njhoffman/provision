ab33879cf230698b67f01a4f67fd97a7856e1942
Nick Hoffman <njhoffman1982@gmail.com>
2023-09-11 19:59:54 -0500

    updates

--- programs/polybar/install.sh
+++ programs/polybar/install.sh
@@ -2,6 +2,7 @@
 
 sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
 sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install libiw-dev libnl-3-dev
 
 mkdir -p ~/.local/src && cd ~/.local/src
 git clone git@github.com:polybar/polybar
